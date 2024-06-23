e75bb3f97e9a9f766852af4b16607dcb7a9585f8
vid-koci <vid.kocijan@gmail.com>
2024-06-23 16:21:38 +0100

    remove some clutter

--- backend/src/rest_api.py
+++ backend/src/rest_api.py
@@ -1,18 +1,13 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import extractor
-import aiofiles  # aiofiles
 
 app = FastAPI()
 
 
 async def check_file(in_file: UploadFile):
     # Process the file and return the result
-    file_path = "tmp_file.docx"
-    async with aiofiles.open(file_path, 'wb') as out_file:
-        content = await in_file.read()  # async read
-        await out_file.write(content)  # async write
-    return extractor.test_a_doc(file_path)
+    return extractor.test_a_doc(in_file.filename)
 
 
 @app.post("/process-file/")
